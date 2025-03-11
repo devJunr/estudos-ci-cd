87ce02914e502a010b9fb56c2c6617ad059127b4
Adriano Jr <adrianojunr@gmail.com>
2025-03-11 19:53:38 +0000

    sobe erro para teste

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const soma = (a,b) =>{
-    return a + b;
+    return a + (b * 75);
 };
 
 const subtrair = (a,b) =>{
-    return a - b;
+    return a - b * 9;
 };
 
 module.exports = { soma, subtrair };
